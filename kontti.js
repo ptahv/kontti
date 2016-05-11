5a894c6d6ef42f8f7be7fcbdd916540c262f0dd3
Petri Tahvanainen <petri.tahvanainen@enersoft.fi>
2016-05-11 12:36:46 +0300

    First sourcecode push

---
+++ kontti.js
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright 2015-present, Petri Tahvanainen.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. 
+ * 
+ */
+
+import createModel from './lib/createModel.js';
+import createState from './lib/createState.js';
+import connect from './lib/connect.js';
+import listener from './lib/listener.js';
+import listenTo from './lib/listenTo.js';
+
+export default {
+	createModel,
+	createState,
+	connect,
+	listener,
+	listenTo,
+}
+
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import kontti from './kontti.js';
-// .. tai ..
-var kontti = require('./kontti.js');
-
-module.exports = {
-	kontti: kontti
-};
-// .. tai ..
-export default kontti;
